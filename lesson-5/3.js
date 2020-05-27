2fa9d3aedfa2017a7c22cc08a6b0b05992609442
Gashenko Anna <janngash@gmail.com>
2020-05-27 15:18:29 +0300

    Finish home work 5

---
+++ lesson-5/3.js
@@ -0,0 +1,26 @@
+/**
+ * Задача 3.
+ *
+ * Создайте функцию `f`, которая отнимает от первого параметра второй и делит на третий.
+ *
+ * Условия:
+ * - Генерировать ошибку, если в качестве любого входного аргумента было предано не число.
+ */
+
+// Решение
+const f = function (a, b, c) {
+    if(typeof a !== 'number'){
+        throw new Error(`Первый аргумент ${a} не число`);
+    } else if(typeof b !== 'number'){
+        throw new Error(`Второй аргумент ${b} не число`);
+    } else if(typeof c !== 'number'){
+        throw new Error(`Третий аргумент ${c} не число`);
+    }
+    const result = (a - b) / c;
+
+    return result;
+};
+
+console.log(f("some", 3, 2)); // 3
+
+exports.f = f;
