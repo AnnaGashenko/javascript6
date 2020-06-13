a245db06d74c0638ebf053c33fb58017d282863a
Gashenko Anna <janngash@gmail.com>
2020-06-13 09:12:01 +0300

    Finish home work 9

---
+++ lesson-9/1.js
@@ -0,0 +1,52 @@
+/**
+ * Задача 1.
+ *
+ * Создайте функцию shallowMerge.
+ * Функция обладает двумя параметрами, каждый из которых должен быть обычным JavaScript объектом.
+ * Функция возвращает новый объект, который в себе объединяет свойства обоих объектов.
+ * Свойства необходимо копировать лишь на один уровень каждого их объектов.
+ *
+ * Из объектов и обеих аргументах копируются только собственные свойства, включая недоступные для перечисления.
+ *
+ * Условия:
+ * - Встроенный метод Object.create() использовать запрещено;
+ * - При копировании любого свойства необходимо обязательно сохранить его дескрипторы;
+ * - Свойства с одинаковыми именами нужно перезаписывать — приоритетом обладает объект из второго параметра.
+ */
+
+// Решение
+
+const user = { firstName: 'Marcus', lastName: 'Kronenberg' };
+const userData = { job: 'developer', country: 'Germany', lastName: 'Schmidt' };
+
+Object.defineProperty(user, 'firstName', { writable: false });
+Object.defineProperty(userData, 'job', { configurable: false });
+
+const setDescriptors = (obj, properies) => {
+    for (const item of Object.entries(properies)) {
+        const key = item[0];
+        const value = item[1];
+        Object.defineProperty(obj, key, value);
+    }
+
+    return obj;
+};
+
+const shallowMerge = function (user, userData) {
+    const userClone = { ...user,  ...userData };
+    const properiesUser = Object.getOwnPropertyDescriptors(user);
+    const properiesUserData = Object.getOwnPropertyDescriptors(userData);
+    const properiesClone = { ...properiesUser, ...properiesUserData };
+
+    const newObj = setDescriptors(userClone, properiesClone);
+
+    return newObj;
+};
+
+const result = shallowMerge(user, userData);
+
+console.log(result); // { firstName: 'Marcus', lastName: 'Schmidt', job: 'developer', country: 'Germany' }
+console.log(Object.getOwnPropertyDescriptor(result, 'firstName').writable); // false
+console.log(Object.getOwnPropertyDescriptor(result, 'job').configurable); // false
+
+exports.shallowMerge = shallowMerge;
