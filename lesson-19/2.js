1782efeec465f86c21b546ee8f391e4cf4ab65b1
Gashenko Anna <janngash@gmail.com>
2020-07-15 22:52:38 +0300

    finish lesson 19

---
+++ lesson-19/2.js
@@ -0,0 +1,73 @@
+/*
+# Задача 2
+
+Создайте функцию `getCustomers` которая умеет объединять 2 массива с объектами.
+
+Операция объединения происходит по ключу `id` и только для объектов у которых установлен флаг `verified: true`.
+
+**Обратите внимание**:
+
+1. Функция `getCustomers` должна возвращать промис;
+2. Использование `async & await` **запрещено**;
+3. Использование посторонних библиотек **запрещено**;
+4. В том случае если в массиве `countries` отсутствует необходимый `id` промис **должен** реджектится с текстом `We don't have information about country for this customer: ${customer.name}`;
+5. Склеивание происходит **только** для объектов с флагом `verified: true`.
+*/
+
+// Решение
+const getCustomers = function (customers, countries) {
+  return new Promise((resolve, reject) => {
+
+      const customerVerified = customers.filter(function(user) {
+          return user.verified === true;
+      });
+
+      const result = [];
+
+      customerVerified.forEach((customer) => {
+          const res = countries.find(function(country) {
+              return country.id === customer.id;
+          });
+
+          if(!res) {
+              reject(`We don't have information about country for this customer: ${customer.name}`);
+          }
+
+          result.push({ ...res, ...customer });
+
+      });
+
+      resolve(result);
+
+  });
+};
+
+// Пример использования
+
+const customers = [
+    {
+        id: 'A1',
+        name: 'Oliver',
+        verified: true
+    },
+    {
+        id: 'A2',
+        name: 'alex'
+    }
+];
+
+const countries = [
+    {
+        id: 'A1',
+        country: 'usa'
+    },
+    {
+        id: 'A2',
+        country: 'poland'
+    }
+];
+
+getCustomers(customers, countries)
+    .then((customers) => console.log(customers))
+    .catch(error => console.log(error));
+
